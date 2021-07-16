528fef35349d9eb59d2ef8f6899d91bb5559427d
Balvinder Bains <53897737+BalBains@users.noreply.github.com>
2021-07-16 16:02:19 +0100

    pokedex app added

---
+++ pokedex/src/bootstrap.tsx
@@ -0,0 +1,45 @@
+import ReactDOM from 'react-dom';
+import {
+  createBrowserHistory,
+  createMemoryHistory,
+  History,
+  LocationListener,
+  LocationState,
+} from 'history';
+
+import App from './App';
+
+interface AppConfig {
+  onNavigate?: LocationListener<LocationState>;
+  initialPath?: string;
+  defaultHistory: History<any> | undefined;
+}
+const mount = (
+  el: Element,
+  { onNavigate, defaultHistory, initialPath = '' }: AppConfig
+) => {
+  const history =
+    defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }: { pathname: string }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_pokedex-dev-root');
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
